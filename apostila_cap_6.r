4456379c052c9b1857308f449a7d3adbb5ea2e72
xschiles <chilesmh@gmail.com>
2024-12-05 20:09:58 -0300

    resolvendo exercicios

--- apostila_cap_6.r
+++ apostila_cap_6.r
@@ -1,11 +1,19 @@
 #1
 #criar função recursiva que retorna n fatorial
 n_fatorial <- function(n) {
-    if (n < 0) {
-        stop("n precisa ser um número natural.")
-    }
-    if (n == 0 || n == 1) {
-        return(1)
-    }
-    return(n * n_fatorial(n - 1))
+  if (n < 0) {
+    stop("n precisa ser um número natural.")
+  }
+  if (n == 0 || n == 1) {
+    return(1)
+  }
+  return(n * n_fatorial(n - 1))
+}
+n_fatorial(10)
+#RESOLVIDO
+
+#2
+#recebe um vetor e retorna o maior elemento dele
+maior_vetor_recursivo <- function(v) {
+    
 }
